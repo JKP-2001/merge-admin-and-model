96e16e7c14195fd485daf2a4fd059dbfcee5fbe8
Anurag <kumarayush1014@gmail.com>
2021-07-12 17:58:15 +0530

    implemented auto setup of profile

--- users/models.py
+++ users/models.py
@@ -3,13 +3,10 @@ from django.contrib.auth.models import User  # 200101017
 
 
 class Profile(models.Model):
-    
-    
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50,default='0')
     roll = models.CharField(max_length=9,default='0')
     batch = models.CharField(max_length=13,default='0')
-    semester = models.CharField(max_length=3,default="0")
     department = models.CharField(max_length=50,default='0')
 
     def __str__(self):
